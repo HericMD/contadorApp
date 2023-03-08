d59104371622201556bb8967b770a6f721fcb7d7
HericMD <hericmdms@gmail.com>
2023-03-08 15:04:35 -0300

    inputs

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,31 +1,45 @@
 import { Component } from "react";
-import { Text, View, Button, StyleSheet } from "react-native";
+import { Text, View, Button, StyleSheet, TextInput } from "react-native";
 
 export default class counter extends Component {
   state = {
     count: 0,
+    step: 12,
   };
 
-  increment(){
-    this.setState({count: this.state.count + 1 });
+  increment() {
+    this.setState({ count: this.state.count + this.state.step });
   }
 
-  decrement(){
-    this.setState({count: this.state.count - 1 });
+  decrement() {
+    this.setState({ count: this.state.count - this.state.step });
   }
 
-  double(){
-    this.setState({count: this.state.count * 2})
+  double() {
+    this.setState({ count: this.state.count * 2 });
   }
 
-  clear(){
-    this.setState({count: this.state.count = 0})
+  clear() {
+    this.setState({ count: (this.state.count = 0) });
+  }
+  alterarStep(texto){
+    this.setState({ step: parseInt(texto) || this.state.step})
   }
 
   render() {
     return (
       <View style={styles.contador}>
-        <Text>Contador: {this.state.count} </Text>
+        <TextInput 
+          value={this.state.step.toString()}
+          onChange={(e) => this.alterarStep(e.nativeEvent.text)}
+          keyboardType="numeric"
+        />
+        <TextInput 
+          defaultValue={this.state.step.toString()}
+          onChangeText={(e) => this.alterarStep(text)}
+          keyboardType="numeric"
+        />
+        <Text> Contador: {this.state.count} </Text>
         <Button onPress={() => this.increment()} title="+" />
         <Text> </Text>
         <Button onPress={() => this.decrement()} title="-" />
